d98b60c4d23a7aef416845b99e4cb32a843bd931
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2020-05-19 14:09:37 -0400

    WIP: Adding basic examples with gene-annotation, axis, and heatmaps

--- src/lib/higlass-lite.ts
+++ src/lib/higlass-lite.ts
@@ -1,116 +1,103 @@
 import Ajv from 'ajv';
-import { HiGlassLiteSpec } from "./higlass-lite.schema";
-import { HiGlassSpec, View as HGView } from "./higlass.schema";
 import HiGlassSchema from "./higlass.schema.json";
+import { HiGlassLiteSpec } from "./higlass-lite.schema";
+import { HiGlassSpec } from "./higlass.schema";
+import { HiGlassModel } from './higlass-model';
+import { HiGlassLiteModel } from './higlass-lite-model';
+import { parseServerAndTilesetUidFromUrl, hgToHlTrackType } from './utils';
 
-function addTrackSourceServer(hg: HiGlassSpec, server: string) {
-    if (hg.trackSourceServers?.indexOf(server) !== -1) hg.trackSourceServers?.push(server);
-    return hg;
-}
+// TODO: Auto-generate readable uids.
 
-export function compile(hl: HiGlassLiteSpec): HiGlassSpec {
+export function compile(ihl: HiGlassLiteSpec): HiGlassSpec {
 
-    let hg: HiGlassSpec = {};
-
-    // TODO: Handle corner cases.
-
-    // TODO: copy data so that we have the original `hl`.
-
-    /**
-     * Fill default values.
-     */
-    // Add default json spec for hl and hg
-    // TODO: add default specs for `hl`
-    for (let v = 0; v < hl.views.length; v++) {
-        if (!hl.views[v].w) hl.views[v].w = 12;
-        if (!hl.views[v].h) hl.views[v].h = 12;
-        if (!hl.views[v].x) hl.views[v].x = 0;
-        if (!hl.views[v].y) hl.views[v].y = 0;
-    }
+    // TODO: Early return with invalidate specs.
     // ...
 
-    // TODO: init default `hg`
-    // ...
+    const hl = new HiGlassLiteModel(ihl);
+    const hg = new HiGlassModel();
 
-    /**
-     * Compile.
-     */
     // config.
-    hg.editable = hl.config?.editable;
-    hg.chromInfoPath = hl.config?.chromInfoPath;
-    hg.trackSourceServers = [];
-    hg.views = [];
+    hg.setEditable(hl.spec().config?.editable)
+        .setChromInfoPath(hl.spec().config?.chromInfoPath);
 
     // views.
-    for (let v = 0; v < hl.views.length; v++) {
-        const view = hl.views[v];
+    for (let v = 0; v < hl.spec().views.length; v++) {
+        const view = hl.spec().views[v];
 
-        let newView: HGView = {
-            layout: {
-                w: view.w as number,
-                h: view.h as number,
-                x: view.x as number,
-                y: view.y as number
-            },
-            tracks: {
-                top: [],
-                left: [],
-                center: [],
-                right: [],
-                bottom: [],
-                gallery: [],
-                whole: []
-            },
-            initialXDomain: [5.960464477539063e-8, 3100000000.0000005] // Default ?
-        };
+        hg.addNewView(view);
 
         for (let t = 0; t < view.tracks.length; t++) {
             const track = view.tracks[t];
 
+            /**
+             * Axis on the top or left.
+             */
             if (track.xAxis === true || track.xAxis === "top") {
-                newView.tracks["top"]?.push({
+                const axisP = track.position === "center" ? "top" : track.position;
+                hg.addTrack(axisP, {
                     type: "horizontal-chromosome-labels",
-                    chromInfoPath: hl.config?.chromInfoPath,
-                    height: 30
+                    chromInfoPath: hl.spec().config?.chromInfoPath,
+                    height: 30 // TODO: default value.
+                });
+            }
+            if (track.yAxis === true || track.yAxis === "left") {
+                const axisP = track.position === "center" ? "left" : track.position;
+                hg.addTrack(axisP, {
+                    type: "vertical-chromosome-labels",
+                    chromInfoPath: hl.spec().config?.chromInfoPath,
+                    height: 30 // TODO: default value.
                 });
             }
 
-            const server = track.data.split("tileset_info/?d=")[0].split("https:")[1];
-            newView.tracks[track.position]?.push({
+            /**
+             * Main track.
+             */
+            const { server, tilesetUid } = parseServerAndTilesetUidFromUrl(track.data);
+            hg.addTrack(track.position, {
                 uid: track.uniqueName,
-                type: track.type,
+                type: hgToHlTrackType(track.type, track.position),
                 server: server,
-                tilesetUid: track.data.split("tileset_info/?d=")[1]
-            });
-            hg.trackSourceServers.push(server);
+                tilesetUid: tilesetUid,
+                width: track.width,
+                height: track.height
+            }).addTrackSourceServers(server);
 
+            /**
+             * Axis on the bottom or right.
+             */
             if (track.xAxis === "bottom") {
-                newView.tracks["bottom"]?.push({
+                const axisP = track.position === "center" ? "bottom" : track.position;
+                hg.addTrack("bottom", {
                     type: "horizontal-chromosome-labels",
-                    chromInfoPath: hl.config?.chromInfoPath,
-                    height: 30
+                    chromInfoPath: hl.spec().config?.chromInfoPath,
+                    height: 30 // TODO: default value.
+                });
+            }
+            if (track.yAxis === true || track.yAxis === "right") {
+                const axisP = track.position === "center" ? "right" : track.position;
+                hg.addTrack(axisP, {
+                    type: "vertical-chromosome-labels",
+                    chromInfoPath: hl.spec().config?.chromInfoPath,
+                    height: 30 // TODO: default value.
                 });
             }
         }
-        hg.views?.push(newView);
     }
 
     // TODO: Validate.
-    validateHG(hg);
+    validateHG(hg.spec());
 
-    return hg;
+    return hg.spec();
 }
 
-export function validateHG(spec: Object): boolean {
+export function validateHG(hg: HiGlassSpec): boolean {
 
     const validate = new Ajv({ extendRefs: true }).compile(HiGlassSchema);
-    const valid = validate(spec);
+    const valid = validate(hg);
 
     if (validate.errors) {
         console.warn(JSON.stringify(validate.errors, null, 2));
     }
-
-    // const hg = spec as HiGlassSpec;
 
     // TODO: check types such as default values and locationLocks
 
