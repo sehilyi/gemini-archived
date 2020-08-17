93f28c20920eebe22ee031c2ac35512709364f0f
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2020-08-17 13:26:39 -0400

    Support rollup

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Editor from "./editor/editor";
-import * as serviceWorker from "./serviceWorker";
+
+import * as monaco from 'monaco-editor';
+
+// Since packaging is done by you, you need
+// to instruct the editor how you named the
+// bundles that contain the web workers.
+self.MonacoEnvironment = {
+  getWorkerUrl: () => './json.worker.bundle.js'
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Editor />
-  </React.StrictMode>,
+    <Editor />,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
